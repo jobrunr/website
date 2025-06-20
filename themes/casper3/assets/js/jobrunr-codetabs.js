45d4cb2d794ff67902f1e09b6e639a9936b8fdd2
wgroeneveld <304593+wgroeneveld@users.noreply.github.com>
2025-06-20 10:12:57 +0200

    serve js in a bundle but keep source separate

---
+++ themes/casper3/assets/js/jobrunr-codetabs.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const tabsContainer = document.querySelectorAll(".tab-container");
+    const contents = document.querySelectorAll(".framework-content");
+    const tabs = document.querySelectorAll(".tab-container .nav-tabs li");
+
+    const setActive = (tabType) => {
+        console.log('tester!!');
+        document.querySelectorAll(`.tab-container .nav-tabs li[data-type="${tabType}"]`).forEach(function(tab) {
+            tab.classList.add('active'); 
+        })
+        document.querySelectorAll(`.framework-content[data-type="${tabType}"]`)?.forEach(function(content) {
+            content.classList.add('active'); 
+        });
+    }
+
+    const removeActive = (nextTabType) => {
+        tabsContainer.forEach(tabContainer => {
+            const activeTab = tabContainer.querySelector(".nav-tabs li.active");
+            if(!activeTab) return;
+            const nextActiveTab = tabContainer.querySelector(`.nav-tabs li[data-type="${nextTabType}`);
+            if(!nextActiveTab) return;
+            activeTab.classList.remove("active");
+            tabContainer.querySelector(".framework-content.active")?.classList.remove("active");
+        })
+    }
+
+    tabs.forEach((tab) => {  
+        tab.addEventListener('click', () => {  
+            const tabType = tab.getAttribute('data-type');
+            console.log('test ron: ', tabType)
+            removeActive(tabType);
+            setActive(tabType);
+        });  
+    }); 
+
+    if (tabs.length > 0) {  
+        setActive(tabs[0].getAttribute('data-type')) 
+    }        
+});
---
+++ themes/casper3/assets/js/jobrunr-framework.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const contents = document.querySelectorAll(".framework-content");
+    const tabs = document.querySelectorAll(".framework-container .nav-tabs li");
+
+    const setActive = (tabType) => {
+        document.querySelectorAll(`.framework-container .nav-tabs li[data-type="${tabType}"]`).forEach(function(tab) {
+            tab.classList.add('active'); 
+        })
+        document.querySelectorAll(`.framework-content[data-type="${tabType}"]`)?.forEach(function(content) {
+            content.classList.add('active'); 
+        }); 
+    }
+
+    const removeActive = (nextTabType) => {
+        document.querySelectorAll(".framework-content.active").forEach(x => x.classList.remove("active"));
+        document.querySelectorAll(".framework-container .nav-tabs li.active").forEach(x => x.classList.remove("active"));
+    }
+
+    tabs.forEach((tab) => {  
+        tab.addEventListener('click', () => {  
+            const tabType = tab.getAttribute('data-type');
+            removeActive(tabType);
+            setActive(tabType);
+        });  
+    }); 
+
+    if (tabs.length > 0) {  
+        setActive(tabs[0].getAttribute('data-type')) 
+    }
+});
