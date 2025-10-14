c9f9ca398a643f7850c7b4d6ddcc53033805db0e
wgroeneveld <304593+wgroeneveld@users.noreply.github.com>
2025-10-14 11:37:09 +0200

    allow direct links to framework types #quarkus etc for guides

--- themes/casper3/assets/js/framework.js
+++ themes/casper3/assets/js/framework.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const contents = document.querySelectorAll(".framework-content");
     const tabs = document.querySelectorAll(".framework-container .nav-tabs li");
+    const possibleTypes = [...document.querySelectorAll(".framework-button")].map(type => type.dataset.type);
 
     const setActive = (tabType) => {
         document.querySelectorAll(`.framework-container .nav-tabs li[data-type="${tabType}"]`).forEach(function(tab) {
@@ -21,10 +22,19 @@ document.addEventListener('DOMContentLoaded', () => {
             const tabType = tab.getAttribute('data-type');
             removeActive(tabType);
             setActive(tabType);
+            window.location.hash = `#${tabType}`
         });  
     }); 
 
     if (tabs.length > 0) {  
         setActive(tabs[0].getAttribute('data-type')) 
     }
-});
+
+    if(window.location.hash) {
+        const possibleType = window.location.hash.substring(1);
+        if(possibleTypes.includes(possibleType)) {
+            removeActive(possibleType);
+            setActive(possibleType);
+        }
+    }
+});  
